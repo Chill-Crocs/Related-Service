593f9a87cce76794bd61728632dca825d31550c9
Christina Santos <christina.santos823@gmail.com>
2021-01-15 23:57:06 -0800

    Get Request created

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 const express = require('express');
+const RelatedItems = require('../database/Item.js');
 
 const app = express();
 const PORT = 3001 || process.env.PORT;
@@ -8,6 +9,20 @@ const PORT = 3001 || process.env.PORT;
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+/// ///Routes//////
+app.get('/api/relatedItems/:id', (req, res) => {
+  const { id } = req.params;
+  RelatedItems.findById(id)
+    .then((response) => {
+      console.log(`Item ${id} successfully retrieved`);
+      res.send(response);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send(err);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
