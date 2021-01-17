b1d4b794ac6778bc2bf64928333e99b9d969df31
Christina Santos <christina.santos823@gmail.com>
2021-01-16 19:32:46 -0800

    Updated rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "plugin:react/recommended",
-        "airbnb"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "ignorePatterns": [
-        "__tests__", 'webpack.config.js'
-    ],
-    "rules": {
-        'no-console': 'off',
-        'no-underscore-dangle': ["error", {"allow": ["_id"]}]
-    }
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  ignorePatterns: [
+    '__tests__', 'webpack.config.js',
+  ],
+  rules: {
+    'no-console': 'off',
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+  },
 };
