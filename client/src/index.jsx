ec3f84101789c46651f35ef5695de45d4ae25cb9
Christina Santos <christina.santos823@gmail.com>
2021-01-26 21:56:08 -0800

    updated CSS style configuration

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Related from './components/Related';
+// eslint-disable-next-line no-unused-vars
+import styles from './components/styles.css';
 
 const randomNumber = () => Math.floor(Math.random() * (99 - 0 + 1)) + 0;
 
--- server/index.js
+++ server/index.js
@@ -5,30 +5,15 @@ const RelatedItems = require('../database/Item.js');
 const app = express();
 const PORT = 3001 || process.env.PORT;
 
-/// ///Middleware//////
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-/// ///Routes//////
 app.get('/api/relatedItems/:id', (req, res) => {
   const { id } = req.params;
   RelatedItems.findById(id)
     .then((response) => {
       console.log(`Item ${id} successfully retrieved`);
-      res.send(response);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.send(err);
-    });
-});
-
-app.patch('/api/relatedItems/:parentId/:favoriteId', (req, res) => {
-  const { parentId, favoriteId } = req.params;
-  const numFavoriteId = Number(favoriteId);
-  RelatedItems.findOneAndUpdate({ _id: parentId, 'relatedItems._id': numFavoriteId }, { $set: { 'relatedItems.$.favorite': true } })
-    .then((response) => {
       res.send(response);
     })
     .catch((err) => {
